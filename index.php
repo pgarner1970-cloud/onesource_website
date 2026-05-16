87476593d8e325078b7641623f67b177e5ada023
pgarner1970-cloud <pgarner1970@googlemail.com>
2026-05-16 10:28:11 +0100

    Main page section breaks

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
 <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
 
-</head><body><div class="topbar">
+</head><body class="home"><div class="topbar">
   <div class="wrap topbar-inner">
     <div class="top-contact">
       <a href="tel:07502216131">☎ 07502 216131</a>
